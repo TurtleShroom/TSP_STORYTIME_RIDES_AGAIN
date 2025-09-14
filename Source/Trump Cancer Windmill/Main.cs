40891348ce48cba75c431013927c6a2730fcbaae
TURTLESHROOM <turtles4all@bellsouth.net>
2025-09-14 17:53:53 -0400

    Major
    1. Fixed misspelled Biome table MAYREQUIRE and Def names!
    
    2. Incorporation of the Trump Windmill Mod (  https://steamcommunity.com/sharedfiles/filedetails/?id=2089966483 )!
    
    3. Fixed issue where the Meffalo's sound effects overrode every other sound in the game.
    
    4. You should now be able to hear the sounds of other animals added by this Mod.
    
    5. Added Dubs Bad Hygine Mod ( https://www.steamcommunity.com/sharedfiles/filedetails/?id=836308268 ) support for various Mod drinks!
    
    6. Added support for the Chia Mod (  https://www.steamcommunity.com/sharedfiles/filedetails/?id=3320114019 ).
    
    7. Patreonlings are more likely to spawn with Combat Enhancing Drugs.
    
    8. Smoothed Asbestos Stone no longer spawns randomly.
    
    9. Borpa Eye Goggles are now more likely to spawn.
    
    10. Incorporated the Astral Dog Mod ( https://www.steamcommunity.com/sharedfiles/filedetails/?id=2727905133 )!
    
    11. Pepsi Frogs are more likely to spawn in cold climates.
    
    12. Fixed multiple corrupted and unclosed Def, Mod Extension, and Comp entries that caused "Rimworld" to fail to load at the Main Menu.
    
    13. Disabled Magic Mod items. I don't have that Mod and am not interested in testing it.

---
+++ Source/Trump Cancer Windmill/Main.cs
@@ -0,0 +1,108 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Trump_Cancer_Windmill;
+using Verse;
+using UnityEngine;
+
+namespace TrumpCancerWindmill
+{
+
+    [StaticConstructorOnStartup]
+    internal static class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("abias1122.TrumpCancerWindmill");
+
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+
+    public class TrumpCancerWindmill : Mod
+    {
+        public static TrumpCancerWindmillSettings Settings;
+
+        public TrumpCancerWindmill(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<TrumpCancerWindmillSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ModLabel".Translate();
+        }
+    }
+
+    [HarmonyPatch(typeof(CompPowerPlantWind), "CompTick")]
+    class CompPowerPlantWindPatch
+    {
+        [HarmonyPostfix]
+        static void Postfix(CompPowerPlantWind __instance)
+        {
+            if (!TrumpCancerWindmill.Settings.disableCancer)
+            {
+                foreach (var pawn in GetPawnsInRadius(__instance))
+                {
+                    if (Rand.Value < TrumpCancerWindmill.Settings.cancerChance)
+                    {
+                        var cancerOnPawn = pawn.health?.hediffSet?.GetFirstHediffOfDef(HediffDefOf.Carcinoma);
+                        var severity = Rand.Range(0.15f, 0.30f);
+
+                        if (cancerOnPawn != null)
+                        {
+                            cancerOnPawn.Severity += severity;
+                            Messages.Message("CancerWorsenedMessage".Translate(pawn.Name), MessageTypeDefOf.NegativeHealthEvent);
+                        }
+                        else
+                        {
+                            var cancer = HediffMaker.MakeHediff(HediffDefOf.Carcinoma, pawn);
+                            cancer.Severity = severity;
+                            pawn.health.AddHediff(cancer);
+
+                            Messages.Message("GotCancerMessage".Translate(pawn.Name), MessageTypeDefOf.NegativeHealthEvent);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Pawn> GetPawnsInRadius(CompPowerPlantWind __instance)
+        {
+            var map = __instance.parent.Map;
+            var cellsToCheck = map.AllCells.Where(cell => cell.DistanceTo(__instance.parent.Position) <= TrumpCancerWindmill.Settings.cancerRadius);
+            return cellsToCheck.Select(cell => cell.GetFirstPawn(map)).Where(pawn => pawn != null);
+        }
+    }
+
+    [HarmonyPatch(typeof(ThingComp), "PostDrawExtraSelectionOverlays")]
+    class ThingCompPatch
+    {
+        [HarmonyPostfix]
+        static void DrawCancerRadius(ThingComp __instance)
+        {
+            if (__instance is CompPowerPlantWind)
+            {
+                GenDraw.DrawRadiusRing(__instance.parent.Position, TrumpCancerWindmill.Settings.cancerRadius);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PlaceWorker_WindTurbine), "DrawGhost")]
+    class PlaceWorker_WindTurbinePatch
+    {
+        [HarmonyPostfix]
+        static void DrawCancerRadius(IntVec3 center)
+        {
+            GenDraw.DrawRadiusRing(center, TrumpCancerWindmill.Settings.cancerRadius);
+        }
+    }
+}
---
+++ Source/Trump Cancer Windmill/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace Trump_Cancer_Windmill
+{
+    public class TrumpCancerWindmillSettings : ModSettings
+    {
+        public int cancerRadius = 20;
+        public float cancerChance = 0.00001f;
+		public bool disableCancer = false;
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref cancerRadius, "cancerRadius", 20, true);
+			Scribe_Values.Look(ref cancerChance, "cancerChance", 0.01f, true);
+		}
+
+		public void DoSettingsWindowContents(Rect inRect)
+		{
+			Listing_Standard list = new Listing_Standard() { ColumnWidth = inRect.width };
+			list.Begin(inRect);
+
+			list.Label("SettingsRadiusLabel".Translate(cancerRadius));
+			cancerRadius = (int)list.Slider(cancerRadius, 1, 100);
+
+			list.Label("SettingsChanceLabel".Translate($"{cancerChance:P3}"));
+			cancerChance = list.Slider(cancerChance, 0.0f, 1.0f);
+
+			list.CheckboxLabeled("SettingsDisableCancerLabel".Translate(), ref disableCancer);
+
+			list.End();
+		}
+	}
+}
